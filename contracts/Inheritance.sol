319b10e3289257141aaa42d2ac07f1c7c79a0e03
Serhii Stanislav <serhiistanislav@gmail.com>
2022-06-28 19:20:16 +0300

    adds Inheritance and Ownable contracts

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+
+abstract contract Balances is Ownable {
+    function getBalance() public view onlyOwner returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(address payable _to) public override virtual {
+        _to.transfer(getBalance());
+    } 
+}
+
+// 1. hardcoded address
+// contract Inheritance is Ownable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), Balances {}
+
+// 2. dynamic address of deployer
+// contract Inheritance is Ownable, Balances {
+//     constructor() Ownable(msg.sender) {}
+// }
+
+// 2. dynamic address of deployer
+contract Inheritance is Ownable, Balances {
+    constructor(address _owner) Ownable(_owner) {}
+
+    function withdraw(address payable _to) public override(Ownable, Balances) onlyOwner {
+        require(_to != address(0), "zero address");
+        Balances.withdraw(_to); // the same: super.withdraw(_to);
+    } 
+}
+
+// https://youtu.be/xkjFcdkAJpg
---
+++ contracts/Ownable.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+
+    constructor(address ownerOverride) {
+        owner = ownerOverride == address(0) ? msg.sender : ownerOverride;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "not an owner");
+        _;
+    }
+
+    function withdraw(address payable _to) public virtual onlyOwner {
+        payable(owner).transfer(address(this).balance);
+
+        _to; // just a hidden warning
+    } 
+}
+
+// https://youtu.be/xkjFcdkAJpg
